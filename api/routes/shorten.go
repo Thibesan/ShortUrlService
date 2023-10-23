8aba3613e571d2854d43c3a5d7ed124336d31e2c
Thibesan Raveenderan <traveen2@uwo.ca>
2023-10-23 14:09:43 -0400

    Input Validation, Secure Service Endpoints
    Template Function Calls for Validator and  HTTP w/ SSL

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -18,3 +18,35 @@ type response struct {
 	XRateRemaining  int           `json:"rate_remaining"`
 	XRateLimitReset time.Duration `json:"rate_limit_reset"`
 }
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+	
+	if err := c.BodyParser(&body); err!= nil {
+		return c.Status(fiber.StatusBadRequest)
+						.JSON(fiber.map{"error":"cannot parse JSON"})
+	}
+
+	//Rate Limiting
+	//Determine User Session by Unique Identifier (IP Address)
+	//Decrement RateRemaining
+	//Output Reset Timer for Refreshes
+	//Proceed with remainder of function call for returning response w/ URL
+
+	//Validate User Input
+
+	//Check if URL is valid
+	if !govalidator.IsURL(body.URL){
+		return c.status(fiber.StatusBadRequest)
+						.JSON(fiber.map{"error":"Invalid URL"})
+	}
+
+	//Check LocalHost domain error (inf loop if source URL is modified)
+	if !helpers.RemoveDomainError(body.URL){
+		return c.Status(fiber.StatusServiceUnavailable)
+						.JSON(fiber.map{"error":"Requested Resource Cannot Be Accessed :)"})
+	}
+
+	//Enforce HTTP, SSL
+	body.URL = helpers.EnforceHTTP(body.URL)
+}
