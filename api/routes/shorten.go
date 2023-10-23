53af372e1dcb23b87328d4f151e135d120170a0a
Thibesan Raveenderan <traveen2@uwo.ca>
2023-10-23 12:58:07 -0400

    Req, Res Structure Definitions
    Define Structures for Requests and Responses within Shorten Service

---
+++ api/routes/shorten.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"time"
+)
+
+
+type request struct {
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
+}
+
+type response struct {
+	URL             string        `json:"url"`
+	CustomShort     string        `json:"short"`
+	Expiry          time.Duration `json:"expiry"`
+	XRateRemaining  int           `json:"rate_remaining"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
